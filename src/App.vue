5c917d6284fa8a58227756ef28a434d94a8e4347
Konstantin-exe <konstantinmuellner@gmail.com>
2021-04-16 14:38:12 +0200

    added b-navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar />
+    <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from './components/NavBar';
+
+export default {
+  name: 'App',
+  components: { NavBar },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -17,16 +26,12 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+a {
+  font-weight: bold;
+  color: #2c3e50;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  &.router-link-exact-active {
+    color: #42b983;
   }
 }
 </style>
